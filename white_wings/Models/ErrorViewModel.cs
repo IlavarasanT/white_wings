8709e90408d66659c900e966453db8e617a015b9
Ilavarasan.T <ilavarasan1623@gmail.com>
2022-10-04 16:09:01 +0530

    Add project files.

---
+++ white_wings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace white_wings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
